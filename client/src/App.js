581ae2e2d3e1dd6a957c1d98ff0ba6c2cc3d7923
Yadhap Dahal <yadhapdahal111@gmail.com>
2019-02-22 15:18:33 -0500

    done

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import "./App.css";
+
+//CSS Styling
+const divStyle = {
+  textAlign: "center",
+  marginTop: "30px",
+  padding: "40px"
+};
+
+const btnDiv = {
+  margin: "10px",
+  fontSize: "35px",
+  fontWeight: "bold",
+  paddingRight: "20px",
+  paddingLeft: "20px"
+};
+
+const counterStyle = {
+  fontSize: "265px",
+  fontWeight: "bold"
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.increaseCounter = this.increaseCounter.bind(this);
+    this.decreaseCounter = this.decreaseCounter.bind(this);
+    this.resetCounter = this.resetCounter.bind(this);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  //Functions
+  increaseCounter() {
+    this.setState(prevState => {
+      return { counter: prevState.counter + 1 };
+    });
+  }
+
+  decreaseCounter() {
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter - 1
+      };
+    });
+  }
+
+  resetCounter() {
+    this.setState(prevState => {
+      return { counter: 0 };
+    });
+  }
+
+  render() {
+    return (
+      <div style={divStyle}>
+        <div style={counterStyle}>{this.state.counter}</div>
+        <button
+          style={btnDiv}
+          className="btn btn-success"
+          onClick={this.increaseCounter}
+        >
+          +
+        </button>
+
+        <button
+          style={btnDiv}
+          className="btn btn-success"
+          onClick={this.resetCounter}
+        >
+          Reset
+        </button>
+        <button
+          style={btnDiv}
+          className="btn btn-success"
+          onClick={this.decreaseCounter}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
